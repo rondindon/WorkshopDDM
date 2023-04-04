911098fc90aa9a46b140d0577049fce2694e94de
rodindon <91866705+rondindon@users.noreply.github.com>
2023-04-04 11:48:12 +0200

    header a zakladna struktura

---
+++ final/_inc/header.php
@@ -0,0 +1,26 @@
+<header>
+
+    <nav class="navbar">
+
+        <div class="logo">
+
+            <img src="img/logo.png" alt="logo" class="logopng">
+
+            <h1 class="title">RONDON WORKSHOP</h1>
+
+        </div>
+
+        <hr class = "nav_line">
+
+        <ul class="nav_links">
+
+            <li class="nav_item"><a href="#">Home</a></li>
+            <li class="nav_item"><a href="#">Our Team</a></li>
+            <li class="nav_item"><a href="#">News & Events</a></li>
+            <li class="nav_item"><a href="#">Book online</a></li>
+
+        </ul>
+
+    </nav>
+
+</header>
