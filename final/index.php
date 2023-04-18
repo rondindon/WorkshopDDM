760f9b821d273faa83cc1c24d1e60f7ac9c183e0
rodindon <91866705+rondindon@users.noreply.github.com>
2023-04-18 11:53:13 +0200

    Add files via upload
    zacatie about sekcie

--- final/index.php
+++ final/index.php
@@ -7,9 +7,60 @@
     <title>Home | Rondon workshop</title>
     <link rel="stylesheet" href="styles/main/style.css">
 </head>
-<body>
+<body class="index-body ">
     
     <?php include "_inc/header.php" ?>
 
+    <div class="title-container">
+
+        <h1 class="title">WELCOME TO RONDON <br> WORKSHOP</h1>
+
+        <h3 class="subtitle">Unforgettable Designs</h3>
+
+    </div>
+
+    <div class="divider shapedividers_com-4950">
+    </div>
+
+    <section class="gallery">
+
+        <div class="photos-container">
+
+            <img src="img/low/sHEMQK.jpg" alt="car-photo" class="photo">
+            <img src="img/low/NI02tn.jpg" alt="car-photo" class="photo">
+            <img src="img/low/uv9ftg.jpg" alt="car-photo" class="photo">
+            <img src="img/low/lowrider-1949-mercury-eight-1urz7dpgq38rj6mw.jpg" alt="car-photo" class="photo">
+            <img src="img/low/ZBI0bj.jpg" alt="car-photo" class="photo">
+            <img src="img/low/LqCxDa.jpg" alt="car-photo" class="photo">
+            <img src="img/low/9U2V31.jpg" alt="car-photo" class="photo">
+            <img src="img/low/Hydraulic_Car.jpg" alt="car-photo" class="photo">
+
+        </div>
+
+    </section>
+
+    <div class="divider shapedividers_com-4897">
+    </div>
+
+    <section class="about">
+
+        <div class="about-container">
+
+            <h1 class="title">ABOUT US</h1>
+
+            <h3 class="subtitle">Creative Old School Designs</h3>
+
+            <p>Welcome to the home of endless creativity. We believe that remarkable designs start with a simple idea and evolve into inspiring outcomes. Rondon workshop’s team of design enthusiasts are wholly committed to your goals and vision. Through our wide range of services we ensure that expectations are not just met, but exceeded. Collaboration and communication are key to long-term success.</p>
+
+            <button class="btn">Get in Touch</button>
+
+            <img src="img/low/jump.jpg" alt="sexy car">
+
+        </div>
+
+    </section>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html>
